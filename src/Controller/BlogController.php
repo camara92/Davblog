c7fb4c01cc66564091eb7c94e634425a92af0428
CAMARA <88945535+camara92@users.noreply.github.com>
2023-03-21 14:14:13 +0100

    creation de mon projet blog du dev et les packages nécessaires au bo,n focntionnement

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/', name: 'app_hello')]
+    public function Hello(): Response
+    {
+        return new Response('<h1>Bonjour Daouda </h1>');
+    }
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+    // routes dynamiques de sf6 :
+    #[Route('/blog/{id}/{nom}', name: 'app_blog')] 
+    public function dynamiques($id, $nom): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'dynamique'=> $id . ' '. $nom
+        ]);
+    }
+}
